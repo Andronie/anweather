22cf1f14c462e76285fd5da35055aa1f57686b3e
An <18864802752@163.com>
2017-07-31 14:07:39 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/anweather/android/db/Province.java
@@ -0,0 +1,29 @@
+package com.anweather.android.db;
+
+/**
+ * Created by An on 2017/7/31.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void  setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+    }
+}
